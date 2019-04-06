a04908753947ba503b2c0af4db3815601b526eaa
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-06 18:51:33 +0300

    Десятый пример третьей главы.

---
+++ src/chapter3/Promote.java
@@ -0,0 +1,24 @@
+package chapter3;
+
+/**
+ * Программа демонстрирующая,
+ * правила продвижения типов.
+ *
+ * @author Ломовской К.Ю.
+ * @since 06.04.2019
+ */
+public class Promote {
+
+    public static void main(String[] args) {
+        byte b = 42;
+        char c = 'a';
+        short s = 1024;
+        int i = 50_000;
+        float f = 5.67f;
+        double d = 10.1234;
+        
+        double result = (f * b) + (i / c) - (d * s);
+        System.out.println((f * b) + " + " + (i / c) + " - " + (d * s));
+        System.out.println("Result = " + result);
+    }
+}
