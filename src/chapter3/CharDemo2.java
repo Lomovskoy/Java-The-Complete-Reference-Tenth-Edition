0c4666e20b334a2e5d25874ff84e8e82a6ccd399
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-06 16:46:36 +0300

    Четвёртый пример третьей главы.

---
+++ src/chapter3/CharDemo2.java
@@ -0,0 +1,22 @@
+package chapter3;
+
+/**
+ * Демонстрация того, что:
+ * символьные переменные ведут себя как целочисленные значения
+ *
+ * @author Ломовской К.Ю.
+ * @since 06.04.2019
+ */
+public class CharDemo2 {
+
+    public static void main(String[] args) {
+        char ch1;
+        
+        ch1 = 'X';
+        System.out.println("ch1 содержит: " + ch1);
+
+        ch1 ++; // Увеличсиваем на единицу значение переменной.
+        System.out.println("ch1++ содержит: " + ch1);
+
+    }
+}
