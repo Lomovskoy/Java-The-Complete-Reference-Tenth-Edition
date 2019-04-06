5cc22d06b56923784fd21638ec81557485040948
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-06 18:10:31 +0300

    Восьмой пример третьей главы.

---
+++ src/chapter3/LifeTime.java
@@ -0,0 +1,21 @@
+package chapter3;
+
+/**
+ * Пример для демонстрации жизненного цикла переменной.
+ *
+ * @author Ломовской К.Ю.
+ * @since 06.04.2019
+ */
+public class LifeTime {
+
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 3; x++){
+            int y = -1;                         // Переменная y инициируется при каждом вхъоде в блок кода.
+            System.out.println("y равно " + y); // Здесь всегда будет выводится -1.
+            y = 100;
+            System.out.println("y теперь равно " + y);
+        }
+    }
+}
