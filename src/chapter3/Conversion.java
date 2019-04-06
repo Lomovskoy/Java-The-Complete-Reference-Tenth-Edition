9da957581254ff4e02fd78b60e814db728a93bf3
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-06 18:33:47 +0300

    Девятый пример третьей главы.

---
+++ src/chapter3/Conversion.java
@@ -0,0 +1,28 @@
+package chapter3;
+
+/**
+ * Демонстрация прпиведенифя типов данных.
+ *
+ * @author Ломовской К.Ю.
+ * @since 06.04.2019
+ */
+public class Conversion {
+
+    public static void main(String[] args) {
+        byte b;
+        int i = 257;
+        double d = 323.142;
+
+        System.out.println("\nПреобразование типа int в тип byte.");
+        b = (byte) i;
+        System.out.println("i и b " + i + " " + b);
+
+        System.out.println("\nПреобразование типа double в тип int.");
+        i = (int) d;
+        System.out.println("d и i " + d + " " + i);
+
+        System.out.println("\nПреобразование типа double в тип byte.");
+        b = (byte) d;
+        System.out.println("d и b " + d + " " + b);
+    }
+}
