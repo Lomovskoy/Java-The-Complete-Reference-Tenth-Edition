c6fb7dac3ba8981ee525d7c8136b68b4c6fa2c79
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-06 11:14:13 +0300

    Третий пример из первой главы.

---
+++ src/chapter2/IfSample.java
@@ -0,0 +1,29 @@
+package chapter2;
+
+/**
+ * Пример демонстрации условного оператора if.
+ * Присвоить исходному файлу имя "IfSample.java"
+ *
+ * @author Ломовской К.Ю.
+ * @since 06.04.2019
+ */
+public class IfSample {
+
+    public static void main(String[] args) {
+        int x, y;
+
+        x = 10;
+        y = 20;
+
+        if (x < y) System.out.println("x меньше y");
+
+        x = x * 2;
+        if (x == y) System.out.println("x равен y");
+
+        x = x * 2;
+        if (x > y) System.out.println("x больше y");
+
+        // Этот оператор не будет ничего выводить
+        if (x == y) System.out.println("Это сообщение не выведится");
+    }
+}
