2be804388abaf6648eaf74e417a03c1f2b6613ca
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-06 15:06:37 +0300

    Четвёрный пример из первой главы.

---
+++ src/chapter2/ForTest.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+/**
+ * Пример демонстрации применения уцикла for.
+ * Присвоить исходному файлу имя "ForTest.java"
+ *
+ * @author Ломовской К.Ю.
+ * @since 06.04.2019
+ */
+public class ForTest {
+
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 10; x = x + 1){
+            System.out.println("Значение x: " + x);
+        }
+    }
+}
