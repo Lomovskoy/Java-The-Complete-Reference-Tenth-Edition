9b5826768e6c4f3549af18866a174bcaac85e3ab
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-06 10:32:40 +0300

    Первый пример из первой главы.

---
+++ src/chapter2/Exemple.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+/**
+ * Это простая программа на Java.
+ * Присвоить исходному файлу имя "Exemple.java"
+ *
+ * @author Ломовской К.Ю.
+ * @since 06.04.2019
+ */
+public class Exemple {
+    // Эта программа начинается с вызова метода main()
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
