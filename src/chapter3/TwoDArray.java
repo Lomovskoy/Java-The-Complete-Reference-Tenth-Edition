621dac211586f26d18f235b3dee952356e87bcee
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-06 22:35:04 +0300

    Четырнадцатый пример третьей главы.

---
+++ src/chapter3/TwoDArray.java
@@ -0,0 +1,28 @@
+package chapter3;
+
+/**
+ * Демонстрация создания двумерного массива.
+ *
+ * @author Ломовской К.Ю.
+ * @since 06.04.2019
+ */
+public class TwoDArray {
+
+    public static void main(String[] args) {
+        int twoD[][] = new int[4][5];
+        int i, j, k = 0;
+
+        for (i = 0; i < 4; i++){
+            for (j = 0; j < 5; j++){
+                twoD[i][j] = k;
+                k++;
+            }
+        }
+
+        for (i = 0; i < 4; i++){
+            for (j = 0; j < 5; j++)
+                System.out.print(twoD[i][j] + "\t");
+            System.out.println();
+        }
+    }
+}
