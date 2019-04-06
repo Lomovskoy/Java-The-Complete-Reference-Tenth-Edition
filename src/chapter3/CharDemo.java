70dcb5721a47ecf48dc7042ccbd15b0b3aeb7c7d
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-06 16:42:23 +0300

    Третий пример третьей главы.

---
+++ src/chapter3/CharDemo.java
@@ -0,0 +1,20 @@
+package chapter3;
+
+/**
+ * Демонстрация переменных типа char.
+ *
+ * @author Ломовской К.Ю.
+ * @since 06.04.2019
+ */
+public class CharDemo {
+
+    public static void main(String[] args) {
+        char ch1, ch2;
+
+        ch1 = 88;   // Код символа X
+        ch2 = 'Y';
+
+        System.out.print("ch1 и ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
