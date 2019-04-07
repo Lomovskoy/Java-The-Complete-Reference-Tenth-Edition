40ad77df1922f67db9759b80eb7a5fcc77b24eee
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-07 10:07:47 +0300

    Шестой пример из четвёртой главы.

---
+++ src/chapter4/ByteShift.java
@@ -0,0 +1,22 @@
+package chapter4;
+
+/**
+ * Программа демонстрирующая,
+ * операция сдвиг в лево, значения byte.
+ *
+ * @author Ломовской К.Ю.
+ * @since 07.04.2019
+ */
+public class ByteShift {
+
+    public static void main(String[] args) {
+        byte a = 64, b;
+        int i;
+
+        i = a << 2;
+        b = (byte) (a << 2);
+
+        System.out.println("Изначальное значение а " + a);
+        System.out.println("i and b: " + i + " " + b);
+    }
+}
