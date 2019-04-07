97e9d9cb3564d333d7da6e818ff8fc8c635d577b
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-07 08:15:54 +0300

    Первый пример из четвёртой главы.

---
+++ src/chapter4/BasicMarh.java
@@ -0,0 +1,43 @@
+package chapter4;
+
+/**
+ * Программа демонстрирующая,
+ * основные арифметические операции.
+ *
+ * @author Ломовской К.Ю.
+ * @since 07.04.2019
+ */
+public class BasicMarh {
+
+    public static void main(String[] args) {
+        // Арифметические операции над целочисленными значениями.
+        System.out.println("Целочисленная арифметика.");
+
+        int a = 1 + 1;
+        int b = a * 3;
+        int c = b / 4;
+        int d = c - a;
+        int e = -d;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+
+        // Арифметические операции над натуральными значениями.
+        System.out.println("\nАрифметика с плавающей точкой.");
+
+        double da = 1 + 1;
+        double db = da * 3;
+        double dc = db / 4;
+        double dd = dc - a;
+        double de = -dd;
+
+        System.out.println("da = " + da);
+        System.out.println("db = " + db);
+        System.out.println("dc = " + dc);
+        System.out.println("dd = " + dd);
+        System.out.println("de = " + de);
+    }
+}
