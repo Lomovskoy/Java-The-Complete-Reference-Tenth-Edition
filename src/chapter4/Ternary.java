66eebcc0fb14cd291086dbaa545307eb946abee9
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-07 12:37:45 +0300

    Двенадцатый пример из четвёртой главы.

---
+++ src/chapter4/Ternary.java
@@ -0,0 +1,26 @@
+package chapter4;
+
+/**
+ * Программа демонстрирующая,
+ * использования тернарного оператора.
+ *
+ * @author Ломовской К.Ю.
+ * @since 07.04.2019
+ */
+public class Ternary {
+
+    public static void main(String[] args) {
+        int i, k;
+        i = 10;
+        k = i < 0 ? -i : i; // Получаем абсалютное значение i
+
+        System.out.print("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+
+        i = -10;
+        k = i < 0 ? -i : i; // Получаем абсалютное значение i
+        
+        System.out.print("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+    }
+}
