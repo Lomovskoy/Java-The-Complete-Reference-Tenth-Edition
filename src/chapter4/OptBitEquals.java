9d334ef089b16a9a9fd2ab28bf071ba9b09add62
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-07 10:47:54 +0300

    Десятый пример из четвёртой главы.

---
+++ src/chapter4/OptBitEquals.java
@@ -0,0 +1,26 @@
+package chapter4;
+
+/**
+ * Программа демонстрирующая,
+ * порязрядные составные операции.
+ *
+ * @author Ломовской К.Ю.
+ * @since 07.04.2019
+ */
+public class OptBitEquals {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        a |= 4;
+        b >>= 1;
+        c <<= 1;
+        a ^= c;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
