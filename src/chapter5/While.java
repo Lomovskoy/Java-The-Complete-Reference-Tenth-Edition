5981c14cd0921acb98bf6056348c544f59e8d38f
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-11 22:55:36 +0300

    Шестой пример из пятой главы.

---
+++ src/chapter5/While.java
@@ -0,0 +1,21 @@
+package chapter5;
+
+/**
+ * Программа демонстрирующая,
+ * применеие цикла while.
+ *
+ * @author Ломовской К.Ю.
+ * @since 11.04.2019
+ */
+public class While {
+
+    public static void main(String[] args) {
+        
+        int n = 10;
+
+        while (n > 0){
+            System.out.println("такт - " + n);
+            n--;
+        }
+    }
+}
