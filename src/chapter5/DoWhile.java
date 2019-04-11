29c440b636c641d94cc2c9049509c1993b0b04de
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-11 23:06:56 +0300

    Восьмой пример из пятой главы.

---
+++ src/chapter5/DoWhile.java
@@ -0,0 +1,20 @@
+package chapter5;
+
+/**
+ * Программа демонстрирующая,
+ * применеие цикла do-while.
+ *
+ * @author Ломовской К.Ю.
+ * @since 11.04.2019
+ */
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        do{
+            System.out.println("такт - " + n);
+        }while (--n > 0);
+    }
+}
