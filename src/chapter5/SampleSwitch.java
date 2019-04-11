fc8c73bc846fcae663c3efed35f8322000c962ac
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-11 22:31:03 +0300

    Второй пример из пятой главы.

---
+++ src/chapter5/SampleSwitch.java
@@ -0,0 +1,34 @@
+package chapter5;
+
+/**
+ * Программа демонстрирующая,
+ * простой пример оператоа Switch.
+ *
+ * @author Ломовской К.Ю.
+ * @since 11.04.2019
+ */
+public class SampleSwitch {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 6; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("i равно нулю.");
+                    break;
+                case 1:
+                    System.out.println("i равно одному.");
+                    break;
+                case 2:
+                    System.out.println("i равно двум.");
+                    break;
+                case 3:
+                    System.out.println("i равно трём.");
+                    break;
+                default:
+                    System.out.println("i больше трёх.");
+            }
+
+        }
+    }
+}
