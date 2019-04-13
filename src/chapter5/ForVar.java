b008786672c509f27c2b2def07b9350ffd760cad
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-13 10:34:07 +0300

    Пятнадцатый пример из пятой главы.

---
+++ src/chapter5/ForVar.java
@@ -0,0 +1,24 @@
+package chapter5;
+
+/**
+ * Программа демонстрирующая, отдельные части
+ * оператора цикла for могут отсудствовать.
+ *
+ * @author Ломовской К.Ю.
+ * @since 13.04.2019
+ */
+public class ForVar {
+
+    public static void main(String[] args) {
+
+        int i;
+        boolean done = false;
+
+        i = 0;
+        for ( ; !done; ){
+            System.out.println("i раво " + i);
+            if (i == 10) done = true;
+            i ++;
+        }
+    }
+}
