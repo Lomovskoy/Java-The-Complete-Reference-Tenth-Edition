02a0d9f6e73f3f73a6c558665f17553bced6153f
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-13 10:16:09 +0300

    Четырнадцатый пример из пятой главы.

---
+++ src/chapter5/Coma.java
@@ -0,0 +1,20 @@
+package chapter5;
+
+/**
+ * Программа демонстрирующая,
+ * использование запятой в операторе цикла for.
+ *
+ * @author Ломовской К.Ю.
+ * @since 13.04.2019
+ */
+public class Coma {
+
+    public static void main(String[] args) {
+        int a, b;
+
+        for (a = 1, b = 4; a < b; a++, b--){
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+        }
+    }
+}
