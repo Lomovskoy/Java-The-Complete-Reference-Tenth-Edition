6021b84a26a99e577a883917306e522c6ec2970d
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-13 09:46:04 +0300

    Десятый пример из пятой главы.

---
+++ src/chapter5/ForTick.java
@@ -0,0 +1,19 @@
+package chapter5;
+
+/**
+ * Программа демонстрирующая,
+ * применеие цикла for.
+ *
+ * @author Ломовской К.Ю.
+ * @since 13.04.2019
+ */
+public class ForTick {
+
+    public static void main(String[] args) {
+
+        int n;
+        
+        for (n = 10; n > 0; n--)
+            System.out.println("такт " + n);
+    }
+}
