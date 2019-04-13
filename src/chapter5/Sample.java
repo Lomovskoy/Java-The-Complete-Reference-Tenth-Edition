f23cf459e85cbc7fdf1203403f6c42e2329f0305
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-13 10:10:03 +0300

    Тринадцатый пример из пятой главы.

---
+++ src/chapter5/Sample.java
@@ -0,0 +1,23 @@
+package chapter5;
+
+/**
+ * Программа демонстрирующая,
+ * управление цикла for несколькими перепменными.
+ *
+ * @author Ломовской К.Ю.
+ * @since 13.04.2019
+ */
+public class Sample {
+
+    public static void main(String[] args) {
+
+        int a, b;
+
+        b = 4;
+        for (a = 1; a < b; a++){
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+            b--;
+        }
+    }
+}
