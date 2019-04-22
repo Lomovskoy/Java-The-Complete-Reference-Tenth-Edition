71e8f7fd937a671e410becd20c89194b57406660
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-22 22:02:36 +0300

    Восемнадцатый пример из пятой главы.

---
+++ src/chapter5/NoChange.java
@@ -0,0 +1,31 @@
+package chapter5;
+
+/**
+ * Программа демонстрирующая, что
+ * переменная в цыкле for each доступна только длячтения.
+ *
+ * @author Ломовской К.Ю.
+ * @since 22.04.2019
+ */
+public class NoChange {
+
+    public static void main(String[] args) {
+
+        int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+
+        // Использовать цикл for в стиле for each дляч вывода
+        // и суммирвоания значений
+        for (int x: nums){
+            System.out.print(x + " ");
+            x *= 10; // Этот оператор никак не повлияет на массив nums
+        }
+
+        System.out.println();
+
+        for (int x: nums)
+            System.out.print(x + " ");
+
+        System.out.println();
+    }
+}
