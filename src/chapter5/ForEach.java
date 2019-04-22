6c830a00f109db6b9487da57549c1923ff273fce
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-22 21:25:39 +0300

    Шестнадцатый пример из пятой главы.

---
+++ src/chapter5/ForEach.java
@@ -0,0 +1,24 @@
+package chapter5;
+
+/**
+ * Программа демонстрирующая,
+ * применение цикла for в стиле for each.
+ *
+ * @author Ломовской К.Ю.
+ * @since 22.04.2019
+ */
+public class ForEach {
+
+    public static void main(String[] args) {
+
+        int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+
+        for (int x: nums){
+            System.out.println("Значение равно: " + x);
+            sum += x;
+        }
+
+        System.out.println("Сумма равна: " + sum);
+    }
+}
