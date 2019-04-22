28b770e5413312ca14af0e4d885d39344122a2a6
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-22 22:18:22 +0300

    Девятнадцатый пример из пятой главы.

---
+++ src/chapter5/ForEach3.java
@@ -0,0 +1,32 @@
+package chapter5;
+/**
+ * Программа демонстрирующая,
+ * применение цикла for для обращения к двумерному массиву.
+ *
+ * @author Ломовской К.Ю.
+ * @since 13.04.2019
+ */
+
+public class ForEach3 {
+
+    public static void main(String[] args) {
+
+        int nums[][] = new int[3][5];
+        int sum = 0;
+
+        // присвоить значение элементам массива nums
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 5; j++)
+                nums[i][j] = (i+1)*(j+1);
+
+        // Использовать цикл for в стиле for each для
+        // вывода и суммирвоания значений
+        for (int x[]: nums){
+            for (int y: x){
+                System.out.println("Значение равно: " + y);
+                sum += y;
+            }
+        }
+        System.out.println("Сумма: " + sum);
+    }
+}
