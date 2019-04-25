8012eb75aa0e6b9dcafbb3e016f41ad223722776
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-25 21:46:46 +0300

    Двадцать восьмой пример из пятой главы.

---
+++ src/chapter5/ContinueLabel.java
@@ -0,0 +1,20 @@
+package chapter5;
+
+/**
+ * Программа демонстрирующая,
+ * применение оператора contine.
+ *
+ * @author Ломовской К.Ю.
+ * @since 25.04.2019
+ */
+public class ContinueLabel {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++){
+            System.out.print(i + " ");
+            if (i % 2 == 0)continue;
+            System.out.println();
+        }
+    }
+}
