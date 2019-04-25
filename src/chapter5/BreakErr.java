eb7255f851d0e2bc9a1131e034234833702c838f
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-25 21:34:05 +0300

    Двадцать седьмой пример из пятой главы.

--- src/chapter5/BreakErr.java
+++ src/chapter5/BreakErr.java
@@ -11,12 +11,12 @@ public class BreakErr {
     public static void main(String[] args) {
 
         one: for (int i = 0; i < 3; i++){
-            System.out.println("Проход " + i + ": ");
+            System.out.print("Проход " + i + ": ");
         }
 
         for (int j = 0; j < 100; j++){
             if (j == 10) break one; // Ошибка!
-            System.out.println(j + " ");
+            System.out.print(j + " ");
         }
     }
 }
