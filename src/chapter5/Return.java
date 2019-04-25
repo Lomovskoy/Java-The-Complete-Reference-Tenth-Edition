b1144d65de7fe4bade60c5479301dcdaabe993ec
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-25 22:06:53 +0300

    Тридцатый пример из пятой главы.

---
+++ src/chapter5/Return.java
@@ -0,0 +1,21 @@
+package chapter5;
+
+/**
+ * Программа демонстрирующая, оператора returm.
+ *
+ * @author Ломовской К.Ю.
+ * @since 25.04.2019
+ */
+public class Return {
+
+    public static void main(String[] args) {
+
+        boolean t = true;
+        System.out.println("До возврата.");
+        
+        if (t) return; // возврат в вызывающицй код
+
+        System.out.println("Этот оператор выполнятьсян не будет");
+    }
+
+}
