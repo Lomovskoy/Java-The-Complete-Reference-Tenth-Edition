e3beb11bd98390503ca272fe16e793924fa881fa
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-25 21:23:39 +0300

    Двадцать шестой пример из пятой главы.

---
+++ src/chapter5/BreakLoop4.java
@@ -0,0 +1,25 @@
+package chapter5;
+
+/**
+ * Программа демонстрирующая,
+ * применение оператора break для вложенных циклов.
+ *
+ * @author Ломовской К.Ю.
+ * @since 22.04.2019
+ */
+public class BreakLoop4 {
+
+    public static void main(String[] args) {
+
+        outer:
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Проход " + i + ": ");
+            for (int j = 0; j < 100; j++) {
+                if (j == 10) break outer; // Выход из обоих циклов
+                System.out.print(j + " ");
+            }
+            System.out.println("Эта строка не будет выведена");
+        }
+        System.out.println("Циклы завершёны.");
+    }
+}
