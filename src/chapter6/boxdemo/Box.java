18c3f8cf47efb86238d8ed9f5bdcfcf1017a6cd3
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-28 09:01:56 +0300

    Первый пример из шестой главы.

---
+++ src/chapter6/boxdemo/Box.java
@@ -0,0 +1,13 @@
+package chapter6.boxdemo;
+
+/**
+ * Программа демонстрирующая, класс модель паралелепипеда.
+ *
+ * @author Ломовской К.Ю.
+ * @since 28.04.2019
+ */
+class Box {
+    double wight;
+    double height;
+    double depth;
+}
---
+++ src/chapter6/boxdemo/BoxDemo.java
@@ -0,0 +1,27 @@
+package chapter6.boxdemo;
+
+/**
+ * Программа демонстрирующая,
+ * использование класаа Box.
+ *
+ * @author Ломовской К.Ю.
+ * @since 28.04.2019
+ */
+public class BoxDemo {
+
+    public static void main(String[] args) {
+        // Объявляется объект типа Box
+        Box myBox = new Box();
+        double vol;
+
+        // Присвоить значение переменным экземпляра класса myBox
+        myBox.wight = 10;
+        myBox.height = 20;
+        myBox.depth = 15;
+
+        // Расчёт объема паралелепипеда
+        vol = myBox.wight * myBox.height * myBox.depth;
+
+        System.out.println("Объем павен " + vol);
+    }
+}
