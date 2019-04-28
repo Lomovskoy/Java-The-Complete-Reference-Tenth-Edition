1b31566ceedc938c5e2a5059c64417b9467ab1b0
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-04-28 13:30:25 +0300

    Четвёртый пример из шестой главы.

--- src/chapter6/boxdemo4/Box4.java
+++ src/chapter6/boxdemo4/Box4.java
@@ -1,20 +1,19 @@
-package chapter6.boxdemo3;
+package chapter6.boxdemo4;
 
 /**
  * Программа демонстрирующая,
- * класс Box с методами.
+ * класс Box с методами возвращающими значение.
  *
  * @author Ломовской К.Ю.
  * @since 28.04.2019
  */
-class Box3 {
+class Box4 {
     double wight;
     double height;
     double depth;
 
     // Вывести объем паралеллепипеда
-    void volume(){
-        System.out.print("Объем равен ");
-        System.out.println(wight * height * depth);
+    double volume(){
+        return wight * height * depth;
     }
 }
---
+++ src/chapter6/boxdemo4/BoxDemo4.java
@@ -0,0 +1,35 @@
+package chapter6.boxdemo4;
+
+/**
+ * Программа демонстрирующая,
+ * использование класаа Box возвращающий значение.
+ *
+ * @author Ломовской К.Ю.
+ * @since 28.04.2019
+ */
+public class BoxDemo4 {
+
+    public static void main(String[] args) {
+        // Объявляется объект типа Box
+        Box4 myBox1 = new Box4();
+        Box4 myBox2 = new Box4();
+        double vol;
+
+        // Присвоить значение переменным экземпляра класса myBox
+        myBox1.wight = 10;
+        myBox1.height = 20;
+        myBox1.depth = 15;
+        // Присвоить другие значения переменным экземпляра класса myBox
+        myBox2.wight = 3;
+        myBox2.height = 6;
+        myBox2.depth = 9;
+
+        // Получить объем первого паралелепипеда
+        vol = myBox1.volume();
+        System.out.println("Объем павен " + vol);
+        
+        // Получить объем второго паралелепипеда
+        vol = myBox2.volume();
+        System.out.println("Объем павен " + vol);
+    }
+}
