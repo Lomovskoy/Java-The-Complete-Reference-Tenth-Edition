5f9408bee895e0de6f353485b59ecc766cd83cec
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-05-02 11:04:14 +0300

    Первый пример из седьмой главы.

--- src/chapter7/overloaddemo/Overload.java
+++ src/chapter7/overloaddemo/Overload.java
@@ -12,6 +12,6 @@ public class Overload {
         ob.test(10);
         ob.test(10, 20);
         result = ob.test(123.25);
-        System.out.println("РЕзультат вызова  ob.test(123.25): " + result);
+        System.out.println("Результат вызова  ob.test(123.25): " + result);
     }
 }
