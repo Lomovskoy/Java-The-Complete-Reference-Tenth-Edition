9adce39cf4388a1a775049dae619c7c7beda00ff
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-05-25 09:27:41 +0300

    Двадцатый пример из седьмой главы.

---
+++ src/chapter7/SpringDemo.java
@@ -0,0 +1,22 @@
+package chapter7;
+
+/**
+ * Программа демонстрирующая,
+ * применение символьных строк.
+ *
+ * @author Ломовской К.Ю.
+ * @since 25.05.2019
+ */
+public class SpringDemo {
+
+    public static void main(String[] args) {
+
+        String strOb1 = "Первая строка";
+        String strOb2 = "Вторая строка";
+        String strOb3 = strOb1 + " и " + strOb2;
+
+        System.out.println(strOb1);
+        System.out.println(strOb2);
+        System.out.println(strOb3);
+    }
+}
