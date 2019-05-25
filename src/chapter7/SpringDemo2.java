17d2964484aa617869c8266380833be57d21fc9d
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-05-25 09:39:35 +0300

    Двадцать первый пример из седьмой главы.

---
+++ src/chapter7/SpringDemo2.java
@@ -0,0 +1,31 @@
+package chapter7;
+
+/**
+ * Программа демонстрирующая,
+ * некоторые методы класса String.
+ *
+ * @author Ломовской К.Ю.
+ * @since 25.05.2019
+ */
+public class SpringDemo2 {
+
+    public static void main(String[] args) {
+
+        String strOb1 = "Первая строка";
+        String strOb2 = "Вторая строка";
+        String strOb3 = strOb1;
+
+        System.out.println("Длинна строки strOb1: " + strOb1.length());
+        System.out.println("Символ по индексу 3 строки strOb1 " + strOb1.charAt(3));
+
+        if (strOb1.equals(strOb2))
+            System.out.println("strOb1 == strOb2");
+        else
+            System.out.println("strOb1 != strOb2");
+
+        if (strOb1.equals(strOb3))
+            System.out.println("strOb1 == strOb3");
+        else
+            System.out.println("strOb1 != strOb3");
+    }
+}
