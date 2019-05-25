5a8cc3e527aa8c76a40b43155f1254c820c5b7d7
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-05-25 15:51:24 +0300

    Третий пример из восьмой главы.

---
+++ src/chapter8/demoboxweight/Box.java
@@ -0,0 +1,45 @@
+package chapter8.demoboxweight;
+
+/**
+ * Программа демонстрирующая, создание супер класса.
+ *
+ * @author Ломовской К.Ю.
+ * @since 25.05.2019
+ */
+class Box {
+
+    double widht;
+    double height;
+    double depth;
+
+    // Сконструировать клон объекта
+    Box(Box ob){ // Передать объект конструктору
+        widht = ob.widht;
+        height = ob.height;
+        depth = ob.depth;
+    }
+
+    // Конструктор, применяемый при указании всех параметров
+    Box(double w, double h, double d){
+        widht = w;
+        height = h;
+        depth = d;
+    }
+
+    // Конструктор, применяемы при отсутствии размеров
+    Box(){
+        widht = -1;
+        height = -1;
+        depth = -1;
+    }
+
+    // Конструктор, применяемый при создании куба
+    Box(double len){
+        widht = height = depth = len;
+    }
+
+    // Расчитать и возвратить объем
+    double volume(){
+        return widht * height * depth;
+    }
+}
---
+++ src/chapter8/demoboxweight/DemoBoxWeight.java
@@ -0,0 +1,27 @@
+package chapter8.demoboxweight;
+
+/**
+ * Программа демонстрирующая, примение
+ * наследование для расширения класса Box().
+ *
+ * @author Ломовской К.Ю.
+ * @since 25.05.2019
+ */
+public class DemoBoxWeight {
+
+    public static void main(String[] args) {
+
+        BoxWeight myBox1 = new BoxWeight(10, 20, 15, 34.3);
+        BoxWeight myBox2 = new BoxWeight(2, 3, 4, 0.076);
+        double vol;
+
+        vol = myBox1.volume();
+        System.out.println("Объем myBox1 = " + vol);
+        System.out.println("Вес myBox1 = " + myBox1.weight);
+        System.out.println();
+
+        vol = myBox2.volume();
+        System.out.println("Объем myBox2 = " + vol);
+        System.out.println("Вес myBox2 = " + myBox2.weight);
+    }
+}
