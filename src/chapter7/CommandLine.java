7473ff41b98a3bcca1964b33a56b4f922d701a30
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-05-25 10:01:14 +0300

    Двадцать третий пример из седьмой главы.

---
+++ src/chapter7/CommandLine.java
@@ -0,0 +1,18 @@
+package chapter7;
+
+/**
+ * Программа демонстрирующая,
+ * выведение всех аргументов командной строки.
+ * При запуске программы 
+ * передать аргументы командной строки: this is a test 100 -1
+ *
+ * @author Ломовской К.Ю.
+ * @since 20.05.2019
+ */
+public class CommandLine {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++)
+            System.out.println("args[" + i + "]: " + args[i]);
+    }
+}
