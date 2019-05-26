f472b2c3b891b5ab282e6ca7c54a71f5e4e949b6
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-05-26 15:53:25 +0300

    Восьмой пример из восьмой главы.

---
+++ src/chapter8/callingcons/C.java
@@ -0,0 +1,15 @@
+package chapter8.callingcons;
+
+/**
+ * Программа демонстрирующая, создание подкласса
+ * путём расширением B класса.
+ *
+ * @author Ломовской К.Ю.
+ * @since 25.05.2019
+ */
+class C extends B {
+
+    C(){
+        System.out.println("В конструкторе C.");
+    }
+}
---
+++ src/chapter8/callingcons/CallingCons.java
@@ -0,0 +1,14 @@
+package chapter8.callingcons;
+
+/**
+ * Программа демонстрирующая, порядок вызова конструкторов.
+ *
+ * @author Ломовской К.Ю.
+ * @since 26.05.2019
+ */
+public class CallingCons {
+
+    public static void main(String[] args) {
+        C c = new C();
+    }
+}
