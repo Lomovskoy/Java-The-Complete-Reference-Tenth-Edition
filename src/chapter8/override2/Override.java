bd5bdcddc74b996677d09f28d9a83d3a110f4b4b
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-05-28 20:57:21 +0300

    Десятый пример из восьмой главы.

---
+++ src/chapter8/override2/Override.java
@@ -0,0 +1,17 @@
+package chapter8.override2;
+
+/**
+ * Программа демонстрирующая,
+ * переопределение метода.
+ *
+ * @author Ломовской К.Ю.
+ * @since 25.05.2019
+ */
+public class Override {
+
+    public static void main(String[] args) {
+        B subOb = new B(1, 2, 3);
+        subOb.show("Это k: "); // Здесь вызываается метод show() из класса В
+        subOb.show();               // Здесь вызываается метод show() из класса A
+    }
+}
