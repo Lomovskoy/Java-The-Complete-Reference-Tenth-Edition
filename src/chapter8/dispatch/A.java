c7a9aa4e034f91b80c3ab59435aaf65a8fb34e49
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-06-09 11:10:36 +0300

    Одинадцатый пример из восьмой главы.

---
+++ src/chapter8/dispatch/A.java
@@ -0,0 +1,15 @@
+package chapter8.dispatch;
+
+/**
+ * Программа демонстрирующая,
+ * динамичскую диспетчеризацию метода.
+ *
+ * @author Ломовской К.Ю.
+ * @since 09.06.2019
+ */
+class A {
+
+    void callme(){
+        System.out.println("В методе callme() класса И");
+    }
+}
---
+++ src/chapter8/dispatch/C.java
@@ -0,0 +1,15 @@
+package chapter8.dispatch;
+
+/**
+ * Программа демонстрирующая,
+ * динамичскую диспетчеризацию метода.
+ *
+ * @author Ломовской К.Ю.
+ * @since 09.06.2019
+ */
+class C extends A{
+
+    void callme(){
+        System.out.println("В методе callme() класса C");
+    }
+}
