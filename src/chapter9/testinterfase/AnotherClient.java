49dee0af43359f9800ebac2929148c334f6cae28
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-06-10 14:49:02 +0300

    Шестой пример из девятой главы.

---
+++ src/chapter9/testinterfase/AnotherClient.java
@@ -0,0 +1,18 @@
+package chapter9.testinterfase;
+
+/**
+ * Программа демонстрирующая,
+ * ещё одна реализацию интерфейса.
+ *
+ * @author Ломовской К.Ю.
+ * @since 10.06.2019
+ */
+public class AnotherClient implements Callback {
+
+    // Реализовать метод интерфейся Callback
+    @Override
+    public void callback(int param) {
+        System.out.println("Ещё один вариант метода callback()");
+        System.out.println("p в квадрате равно " + param);
+    }
+}
---
+++ src/chapter9/testinterfase/TestInterfase2.java
@@ -0,0 +1,22 @@
+package chapter9.testinterfase;
+
+/**
+ * Программа демонстрирующая,
+ * доступ к реализхации через ссылки на интерфейсы.
+ *
+ * @author Ломовской К.Ю.
+ * @since 10.06.2019
+ */
+public class TestInterfase2 {
+
+    public static void main(String[] args) {
+
+        Callback c = new Client();
+        AnotherClient ob = new AnotherClient();
+
+        c.callback(42);
+        c = ob;                 // Теперь переменная ссылается на объект типа AnotherClient
+
+        c.callback(42);
+    }
+}
