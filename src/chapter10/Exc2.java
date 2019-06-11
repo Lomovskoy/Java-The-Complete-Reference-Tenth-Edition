8116eb30f4e5b622de9fe9a8eceb42546cae6699
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-06-11 16:20:39 +0300

    Второй пример из десятой главы.

---
+++ src/chapter10/Exc2.java
@@ -0,0 +1,24 @@
+package chapter10;
+
+/**
+ * Программа демонстрирующая,
+ * пример обрабатываемого исключения.
+ *
+ * @author Ломовской К.Ю.
+ * @since 10.06.2019
+ */
+public class Exc2 {
+
+    public static void main(String[] args) {
+        int d, a;
+
+        try {
+            d = 0;
+            a = 42 / d;
+            System.out.println("Это не будет выведенно.");
+        }catch (ArithmeticException e){
+            System.out.println("Деление на ноль");
+        }
+        System.out.println("Посале оператора catch");
+    }
+}
