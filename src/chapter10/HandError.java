cef5d54ce243802b6292c915261e302455f86234
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-06-11 16:33:21 +0300

    Третий пример из десятой главы.

---
+++ src/chapter10/HandError.java
@@ -0,0 +1,31 @@
+package chapter10;
+
+import java.util.Random;
+
+/**
+ * Программа демонстрирующая, пример обработки
+ * исключения и продолжения работы.
+ *
+ * @author Ломовской К.Ю.
+ * @since 10.06.2019
+ */
+public class HandError {
+
+    public static void main(String[] args) {
+
+        int a = 0, b = 0, c = 0;
+        Random r = new Random();
+
+        for (int i = 0; i < 32000; i++){
+            try {
+                b = r.nextInt();
+                c = r.nextInt();
+                a = 12345 / (b / c);
+            } catch (ArithmeticException e){
+                System.out.println("Деление на ноль.");
+                a = 0;
+            }
+            System.out.println("a: " + a);
+        }
+    }
+}
