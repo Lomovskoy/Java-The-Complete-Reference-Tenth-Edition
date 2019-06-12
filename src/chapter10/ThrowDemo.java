1c372d1ba7d0d55b3f9f021a0444c2c9ae0f5aaa
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-06-12 12:11:05 +0300

    Девятый пример из десятой главы.

---
+++ src/chapter10/ThrowDemo.java
@@ -0,0 +1,28 @@
+package chapter10;
+
+/**
+ * Программа демонстрирующая,
+ * применение оператора throw.
+ *
+ * @author Ломовской К.Ю.
+ * @since 12.06.2019
+ */
+public class ThrowDemo {
+
+    static void demoProc() {
+        try {
+            throw new NullPointerException("Демонстрация");
+        }catch (NullPointerException e){
+            System.out.println("Исключение перехваченно в теле метода demoProc()");
+            throw e;    // Повторне сгенерированно исключение
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            demoProc();
+        }catch (NullPointerException e){
+            System.out.println("Повторный перехват: " + e);
+        }
+    }
+}
