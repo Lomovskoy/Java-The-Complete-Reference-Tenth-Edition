8f0326d09cb05be07ac51c7c0ba7725be8a5eed8
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-06-15 14:10:53 +0300

    Седьмой пример из двенадцатой главы.

--- src/chapter12/AutoBox.java
+++ src/chapter12/AutoBox.java
@@ -2,18 +2,20 @@ package chapter12;
 
 /**
  * Программа демонстрирующая,
- * оболочки числового типа.
+ * автоупаковку и автораспаковку.
  *
  * @author Ломовской К.Ю.
  * @since 15.06.2019
  */
-public class Wrap {
+public class AutoBox {
 
     public static void main(String[] args) {
 
-        Integer iOb = new Integer(100);
+        // Автоупаковка значениятипа int
+        Integer iOb = 100;
 
-        int i = iOb.intValue();
+        // Автораспаковка значениятипа int
+        int i = iOb;
 
         // Выведет значение 100 100
         System.out.println(i + " " + iOb);
