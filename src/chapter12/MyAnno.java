74b2c107d30b0fbe01e163e334b3910337c933ce
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-06-15 16:28:29 +0300

    Четырнадцатый пример из двенадцатой главы.

--- src/chapter12/MyAnno.java
+++ src/chapter12/MyAnno.java
@@ -7,7 +7,7 @@ package chapter12;
  * @author Ломовской К.Ю.
  * @since 15.06.2019
  */
-@interface MyAno {
+@interface MyAnno {
 
     String atr();
 
---
+++ src/chapter12/MyAnnoRun.java
@@ -0,0 +1,19 @@
+package chapter12;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Программа демонстрирующая, удержание класса
+ * MyAnnoRun со степенью сохранности RUNTIME.
+ *
+ * @author Ломовской К.Ю.
+ * @since 15.06.2019
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnnoRun {
+
+    String atr();
+
+    int val();
+}
