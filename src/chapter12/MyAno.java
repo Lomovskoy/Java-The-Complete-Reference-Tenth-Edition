d6b6bbcd22dff3ceaf60b7ab4f972738b6bdbd2f
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-06-15 15:46:22 +0300

    Тринадцатый пример из двенадцатой главы.

---
+++ src/chapter12/MyAno.java
@@ -0,0 +1,15 @@
+package chapter12;
+
+/**
+ * Программа демонстрирующая,
+ * простой тип аннотации.
+ *
+ * @author Ломовской К.Ю.
+ * @since 15.06.2019
+ */
+@interface MyAno {
+
+    String atr();
+
+    int val();
+}
