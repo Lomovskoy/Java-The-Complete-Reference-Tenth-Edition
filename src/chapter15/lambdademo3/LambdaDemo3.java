8331ea24689c45ebe8e2478d6dde66ed5ee50412
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-07-20 12:15:57 +0300

    Третий пример из пятнадцатой главы.

--- src/chapter15/lambdademo3/LambdaDemo3.java
+++ src/chapter15/lambdademo3/LambdaDemo3.java
@@ -11,6 +11,8 @@ class LambdaDemo3 {
 
     public static void main(String[] args) {
 
+        // В этом лямбда-выражении проверяется,
+        // является ли одно число множителем другого
         NumericTest2 isFactor = (n, d) -> (n % d) == 0;
 
         if (isFactor.test(10, 2))
