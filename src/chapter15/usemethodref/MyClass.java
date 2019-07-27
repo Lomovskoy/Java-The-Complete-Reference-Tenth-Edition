0f9dae1bedfd10c74c1b6385669cbfb2a9060a65
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-07-27 13:49:44 +0300

    Четырнадцатый пример из пятнадцатой главы.

---
+++ src/chapter15/usemethodref/MyClass.java
@@ -0,0 +1,17 @@
+package chapter15.usemethodref;
+
+/**
+ * Класс демонстрирующий ссылку на метод,
+ * что бынайти максимальное значение в коллекции.
+ *
+ * @author Ломовской К.Ю.
+ * @since 27.07.2019
+ */
+class MyClass {
+
+    private int val;
+
+    MyClass(int v) { val = v; }
+
+    int getVal() { return val;}
+}
---
+++ src/chapter15/usemethodref/UseMethoDref.java
@@ -0,0 +1,30 @@
+package chapter15.usemethodref;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class UseMethoDref {
+
+    static int compareMC(MyClass a, MyClass b){
+        return a.getVal() - b.getVal();
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<MyClass> al = new ArrayList<MyClass>();
+
+        al.add(new MyClass(1));
+        al.add(new MyClass(4));
+        al.add(new MyClass(2));
+        al.add(new MyClass(9));
+        al.add(new MyClass(3));
+        al.add(new MyClass(7));
+
+        // Найти максимальное значение, используя метод
+        MyClass maxValObj = Collections.max(al, UseMethoDref::compareMC);
+
+        System.out.println("Максимальное значение равно: " + maxValObj.getVal());
+    }
+
+}
