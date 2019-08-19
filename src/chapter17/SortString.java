5c5f34141cfef0064e4af1722a472b51994b0c2b
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-08-19 11:23:57 +0300

    Девятый пример из семнадцатой главы.

---
+++ src/chapter17/SortString.java
@@ -0,0 +1,31 @@
+package chapter17;
+
+/**
+ * Класс демонстрирующий пузырьковую
+ * сортировку объектов типа String()
+ * по их лексиграфическому значению.
+ *
+ * @author Ломовской К.Ю.
+ * @since 19.08.2019
+ */
+class SortString {
+
+    static String[] arr = {
+            "Now", "is", "the", "time", "for", "all",
+            "good", "men", "to", "come", "to", "the",
+            "aid", "of", "their", "country"
+    };
+
+    public static void main(String[] args) {
+        for (int j = 0; j < arr.length; j++){
+            for (int i = j + 1; i < arr.length; i++){
+                if (arr[i].compareTo(arr[j]) < 0){
+                    String t = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = t;
+                }
+            }
+            System.out.println(arr[j]);
+        }
+    }
+}
