60aea8037d96aae5d5b0d7e02237e9caa096b654
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-08-19 11:47:37 +0300

    Одинадцатый пример из семнадцатой главы.

---
+++ src/chapter17/StringReplase.java
@@ -0,0 +1,33 @@
+package chapter17;
+/**
+ * Класс демонстрирующий замену
+ * подстроки в строке.
+ *
+ * @author Ломовской К.Ю.
+ * @since 19.08.2019
+ */
+
+class StringReplase {
+
+    public static void main(String[] args) {
+
+        String org = "This is a test. This is too.";
+        String search = "is";
+        String sub = "was";
+        String result;
+
+        int i;
+
+        // Заменить все совпадения подстроки
+        do {
+            System.out.println(org);
+            i = org.indexOf(search);
+            if (i != -1){
+                result = org.substring(0, i);
+                result = result + sub;
+                result = result + org.substring(i + search.length());
+                org = result;
+            }
+        }while (i != -1);
+    }
+}
