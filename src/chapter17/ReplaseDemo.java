e1ba946937c7f36458ab6ef308f4ad879c5bd74b
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-08-20 10:03:12 +0300

    Двадцать первый пример из семнадцатой главы.

--- src/chapter17/ReplaseDemo.java
+++ src/chapter17/ReplaseDemo.java
@@ -12,7 +12,7 @@ class ReplaseDemo {
     public static void main(String[] args) {
 
         StringBuffer sb = new StringBuffer("Это простой тест.");
-        sb.replace(4, 8, "был");
+        sb.replace(4, 11, "был");
         System.out.println("После замены: " + sb);
     }
 }
