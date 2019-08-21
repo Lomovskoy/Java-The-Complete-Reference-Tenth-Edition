64c0d8e8f913c3f49b9ad0ef91bed0f439e55114
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-08-21 10:39:00 +0300

    Первый пример из восемнадцатой главы.

---
+++ src/chapter18/DoubleDemo.java
@@ -0,0 +1,19 @@
+package chapter18;
+
+/**
+ * Класс демонстрирующий создание
+ * класса Double двумя сповобами.
+ *
+ * @author Ломовской К.Ю.
+ * @since 21.08.2019
+ */
+class DoubleDemo {
+
+    public static void main(String[] args) {
+
+        Double d1 = Double.valueOf(3.14159);
+        Double d2 = Double.valueOf("314159E-5");
+        System.out.println(d1 + " = " + d2 + " -> " + d1.equals(d2));
+        
+    }
+}
