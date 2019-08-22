357ae51736444a58712188a4089f118bf25dd7f2
Lomovskoy <lomovskoy.kirill@yandex.ru>
2019-08-22 11:15:43 +0300

    Тринадцатый пример из восемнадцатой главы.

---
+++ src/chapter18/ShowUserInfo.java
@@ -0,0 +1,14 @@
+package chapter18;
+
+/**
+ * Класс демонстрирующий применение Property.
+ *
+ * @author Ломовской К.Ю.
+ * @since 22.08.2019
+ */
+class ShowUserInfo {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
