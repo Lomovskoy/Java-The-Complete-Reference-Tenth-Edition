1d6d6039644e3aefbf615ace20595b339d0b1810
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-02-09 16:40:28 +0300

    Девятый пример из двадцатой главы

---
+++ src/chapter20/CurDemo.java
@@ -0,0 +1,25 @@
+package chapter20;
+
+import java.util.Currency;
+import java.util.Locale;
+
+/**
+ * Класс демонстрирующий применение
+ * класса Currency.
+ *
+ * @author Ломовской К.Ю.
+ * @since 09.02.2020
+ */
+public class CurDemo {
+
+    public static void main(String[] args) {
+
+        Currency currency;
+
+        currency = Currency.getInstance(Locale.US);
+
+        System.out.println("Символ: " + currency.getSymbol());
+        System.out.println("Количество цифр в дробной части " +
+                "числа по умолчанию: " + currency.getDefaultFractionDigits());
+    }
+}
