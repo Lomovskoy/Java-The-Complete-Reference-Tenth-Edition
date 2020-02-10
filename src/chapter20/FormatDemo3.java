745a2b506127b7aaf699326bf7a3f03c13927849
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-02-10 23:13:00 +0300

    Тринадцатый пример из двадцатой главы

---
+++ src/chapter20/FormatDemo3.java
@@ -0,0 +1,23 @@
+package chapter20;
+
+import java.util.Formatter;
+
+/**
+ * Класс демонстрирующий применение
+ * спецификаторов формата %n и %%.
+ *
+ * @author Ломовской К.Ю.
+ * @since 09.02.2020
+ */
+public class FormatDemo3 {
+
+    public static void main(String[] args) {
+
+        Formatter formatter = new Formatter();
+
+        formatter.format("Копирование файла%nПередача завершена на %d%%", 88);
+        System.out.println(formatter);
+
+        formatter.close();
+    }
+}
