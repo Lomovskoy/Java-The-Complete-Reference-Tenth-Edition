c0c76ce4f9272c7abe3fc703b3a41d5289bd00db
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-02-26 23:07:07 +0300

    Семнадцатый пример из двадцатой первой главы

---
+++ src/chapter21/CharArrayReadDemo.java
@@ -0,0 +1,45 @@
+package chapter21;
+
+import java.io.CharArrayReader;
+import java.io.IOException;
+
+/**
+ * Класс демонстрирующий использование
+ * класса CharArrayRead.
+ *
+ * @author Ломовской К.Ю.
+ * @since 26.02.2020
+ */
+public class CharArrayReadDemo {
+
+    public static void main(String[] args) {
+
+        String tmp = "abcdefghijklmnopqrstuvwxyz";
+        int length = tmp.length();
+        char[] c = new char[length];
+
+        tmp.getChars(0, length, c, 0);
+        int i;
+
+        try (CharArrayReader input1 = new CharArrayReader(c)){
+            System.out.println("input1:");
+            while ((i = input1.read()) != -1){
+                System.out.print((char) i);
+            }
+            System.out.println();
+        } catch (IOException e){
+            System.out.println("Ошибка ввода-вывода: " + e);
+        }
+
+        try (CharArrayReader input2 = new CharArrayReader(c, 0, 5)){
+            System.out.println("input2:");
+            while ((i = input2.read()) != -1){
+                System.out.print((char) i);
+            }
+            System.out.println();
+        } catch (IOException e){
+            System.out.println("Ошибка ввода-вывода: " + e);
+        }
+
+    }
+}
