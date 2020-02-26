9b797d67fed0cb8fd96d8d2c33930199e73c6394
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-02-26 23:20:38 +0300

    Девятнадцатый пример из двадцатой первой главы

---
+++ src/chapter21/BufferedReaderDemo.java
@@ -0,0 +1,62 @@
+package chapter21;
+
+import java.io.*;
+
+/**
+ * Класс демонстрирующий использование
+ * буферезированного ввода.
+ *
+ * @author Ломовской К.Ю.
+ * @since 26.02.2020
+ */
+public class BufferedReaderDemo {
+
+    public static void main(String[] args) {
+
+        String s = "Это знак авторского права &copy; , а &copy нет.\n";
+        char[] buf = new char[s.length()];
+        s.getChars(0, s.length(), buf, 0);
+
+        CharArrayReader in = new CharArrayReader(buf);
+        int c;
+        boolean marked = false;
+
+        try (BufferedReader f = new BufferedReader(in)){
+            while ((c = f.read()) != -1){
+                switch (c){
+                    case '&':
+                        if (!marked){
+                            f.mark(32);
+                            marked = true;
+                        } else {
+                            marked = false;
+                        }
+                        break;
+                    case ';':
+                        if (marked){
+                            marked = false;
+                            System.out.print("(c)");
+                        } else {
+                            System.out.print((char) c);
+                        }
+                        break;
+                    case ' ':
+                        if (marked){
+                            marked = false;
+                            f.reset();
+                            System.out.print("&");
+                        } else {
+                            System.out.print((char) c);
+                        }
+                        break;
+                    default:
+                        if (!marked)
+                            System.out.print((char) c);
+                        break;
+                }
+            }
+        } catch (IOException e){
+            System.out.println("Ошибка ввода-вывода: " + e);
+        }
+    }
+}
