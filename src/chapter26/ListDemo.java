d394320a908d0c1eea56312186b8d2fcc1b27a71
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-03-05 23:15:43 +0300

    Седьмой пример из двадцатой шестой главы

--- src/chapter26/ListDemo.java
+++ src/chapter26/ListDemo.java
@@ -1,31 +1,28 @@
 package chapter26;
 
 import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 /**
- * Класс демонстрирующий применение
- * раскрывающихся списков.
+ * Класс демонстрирующий применение списков.
  *
  * @author Ломовской К.Ю.
  * @since 05.03.2020
  */
-public class ChoiceDemo extends Frame implements ItemListener {
+public class ListDemo extends Frame implements ActionListener {
 
-    Choice os, browser;
+    List os, browser;
     String msg = "";
 
-    public ChoiceDemo(){
-
+    public ListDemo(){
         // Использовать диспетчер полной компановки
         setLayout(new FlowLayout());
 
-        // Создать раскрывающиеся списки
-        os = new Choice();
-        browser = new Choice();
+        // Создать список с множественным выбором
+        os = new List(4, true);
+
+        // Создать список с одинарным выбором
+        browser = new List(4);
 
         // Ввести элементы в список os
         os.add("Windows");
@@ -38,13 +35,17 @@ public class ChoiceDemo extends Frame implements ItemListener {
         browser.add("Firefox");
         browser.add("Chrome");
 
-        // Ввести раскрывающиеся списки в окно
+        // Сделать первоначальный выбор
+        browser.select(1);
+        os.select(0);
+
+        // Ввести списки в окно
         add(os);
         add(browser);
 
         // Ввести приёмнткти событий в отдельных элементах
-        os.addItemListener(this);
-        browser.addItemListener(this);
+        os.addActionListener(this);
+        browser.addActionListener(this);
 
         addWindowListener(new WindowAdapter() {
             @Override
@@ -55,24 +56,30 @@ public class ChoiceDemo extends Frame implements ItemListener {
     }
 
     @Override
-    public void itemStateChanged(ItemEvent e) {
+    public void actionPerformed(ActionEvent e) {
         repaint();
     }
 
     @Override // Отобразить текуцщее варианты выбора
     public void paint(Graphics g) {
+        int[] idx;
+
         msg = "Current OS: ";
-        msg += os.getSelectedItem();
-        g.drawString(msg, 20, 120);
+        idx = os.getSelectedIndexes();
+
+        for (int i : idx)
+            msg += os.getItem(i) + " ";
+
+        g.drawString(msg, 6, 120);
         msg = "Current Browser: ";
         msg += browser.getSelectedItem();
-        g.drawString(msg, 20, 140);
+        g.drawString(msg, 6, 140);
     }
 
     public static void main(String[] args) {
-        ChoiceDemo appWin = new ChoiceDemo();
-        appWin.setSize(new Dimension(240, 180));
-        appWin.setTitle("ChoiceDemo");
+        ListDemo appWin = new ListDemo();
+        appWin.setSize(new Dimension(300, 180));
+        appWin.setTitle("ListDemo");
         appWin.setVisible(true);
     }
 }
