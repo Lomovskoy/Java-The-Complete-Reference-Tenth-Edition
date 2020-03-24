30efa809e1fe60afee3c2bd5702e988d2e2b1959
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-03-24 23:47:07 +0300

    Девятый пример из двадцать девятой главы

--- src/chapter29/StreamDemo9.java
+++ src/chapter29/StreamDemo9.java
@@ -7,7 +7,7 @@ import java.util.stream.Stream;
 
 /**
  * В этой программе демонстрируется применение
- * итератора для потока данных.
+ * итератора-разделителя для потока данных.
  *
  * @author Ломовской К.Ю.
  * @since 24.03.2020
@@ -34,5 +34,11 @@ public class StreamDemo9 {
         // Перебрать элементы в потоке данных
         while (splitItr.tryAdvance((n)-> System.out.println(n)));
         // while (splitItr.tryAdvance(System.out::println));
+
+        System.out.println();
+        myStream = myList.stream();
+        splitItr = myStream.spliterator();
+        splitItr.forEachRemaining((n)-> System.out.println(n));
+        // splitItr.forEachRemaining(System.out::println);
     }
 }
