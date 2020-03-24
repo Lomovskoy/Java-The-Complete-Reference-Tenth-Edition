43c6f4af767fbd7dda8b837d2da0af5d49512911
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-03-24 23:41:07 +0300

    Девятый пример из двадцать девятый главы

---
+++ src/chapter29/StreamDemo9.java
@@ -0,0 +1,38 @@
+package chapter29;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+
+/**
+ * В этой программе демонстрируется применение
+ * итератора для потока данных.
+ *
+ * @author Ломовской К.Ю.
+ * @since 24.03.2020
+ */
+public class StreamDemo9 {
+
+    public static void main(String[] args) {
+
+        // Создать список символьных строк
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("Альфа");
+        myList.add("Бета");
+        myList.add("Гамма");
+        myList.add("Дельта");
+        myList.add("Кси");
+        myList.add("Омега");
+
+        // Получить поток данных из писочного массива
+        Stream<String> myStream = myList.stream();
+
+        // Получить итератор-разделитель для потока данных
+        Spliterator<String> splitItr = myStream.spliterator();
+
+        // Перебрать элементы в потоке данных
+        while (splitItr.tryAdvance((n)-> System.out.println(n)));
+        // while (splitItr.tryAdvance(System.out::println));
+    }
+}
