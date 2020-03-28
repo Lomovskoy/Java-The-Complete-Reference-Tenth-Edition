a289a68938ec254dd4be5b7c4a426c1b6323720c
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-03-28 13:54:57 +0300

    Восьмой пример из тридцатой главы

---
+++ src/chapter30/RegExpr8.java
@@ -0,0 +1,26 @@
+package chapter30;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * В этой программе демонстрируется
+ * применение класса символов.
+ *
+ * @author Ломовской К.Ю.
+ * @since 28.03.2020
+ */
+public class RegExpr8 {
+
+    public static void main(String[] args) {
+
+        String str = "Jon Jonathan Frank Ken Todd";
+        Pattern pat = Pattern.compile("Jon.*? ");
+        Matcher mat = pat.matcher(str);
+
+        System.out.println("Исходная последовательность: " + str);
+        str = mat.replaceAll("Eric ");
+
+        System.out.println("Изменённая последовательтность: " + str);
+    }
+}
