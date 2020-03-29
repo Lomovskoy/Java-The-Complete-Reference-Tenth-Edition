b2ce27c542907b12e30c1cf771962b6aad7723f2
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-03-29 18:23:30 +0300

    Семнадцатый пример из тридцатой главы

---
+++ src/chapter30/DateTimeDemo2.java
@@ -0,0 +1,25 @@
+package chapter30;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+/**
+ * В этой программе демонстрируются
+ * применение класса DateTimeFormatter.
+ *
+ * @author Ломовской К.Ю.
+ * @since 29.03.2020
+ */
+public class DateTimeDemo2 {
+
+    public static void main(String[] args) {
+
+        LocalDate curDate = LocalDate.now();
+        System.out.println(curDate.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
+
+        LocalDateTime curTime = LocalDateTime.now();
+        System.out.println(curDate.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)));
+    }
+}
