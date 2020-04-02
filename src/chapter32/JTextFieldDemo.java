91cd942c344e2e5b5467d01de54496ed63e66b3d
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-04-02 21:06:04 +0300

    Второй пример из тридцать второй главы

---
+++ src/chapter32/JTextFieldDemo.java
@@ -0,0 +1,44 @@
+package chapter32;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * В этой программе демонстрируются
+ * примение компонентов типа JTextField.
+ *
+ * @author Ломовской К.Ю.
+ * @since 02.04.2020
+ */
+public class JTextFieldDemo {
+
+    public JTextFieldDemo(){
+        // Установить фрейм средствами класаса JFrame
+        JFrame jFrame = new JFrame("JTextFieldDemo");
+        jFrame.setLayout(new FlowLayout());
+        jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        jFrame.setSize(260, 120);
+
+        // Ввести текстовое поле в панель содержимого
+        JTextField jTextField = new JTextField(15);
+        jFrame.add(jTextField);
+
+        // Ввести метку
+        JLabel jLabel = new JLabel();
+        jFrame.add(jLabel);
+
+        // Обработать события действия. Отобразить
+        // текст, когда пользовательно нажмёт ввод
+        jTextField.addActionListener(e -> jLabel.setText(jTextField.getText()));
+
+        // Отобразить фрейм
+        jFrame.setVisible(Boolean.TRUE);
+    }
+
+    public static void main(String[] args) {
+        // Создать фрейм в потоке деспетчиризации событий
+        SwingUtilities.invokeLater(JTextFieldDemo::new);
+    }
+}
