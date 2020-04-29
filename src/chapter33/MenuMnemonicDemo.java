9ba0f864a915e2d929925a4bd614ffef20af9e66
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-04-29 18:27:01 +0300

    Второй пример из тридцать третьей главы

--- src/chapter33/MenuMnemonicDemo.java
+++ src/chapter33/MenuMnemonicDemo.java
@@ -4,21 +4,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 /**
  * В этой программе демонстрируются
- * простое главное меню.
+ * простое главное меню с использованием мнемоников.
  *
  * @author Ломовской К.Ю.
  * @since 28.04.2020
  */
-public class MenuDemo implements ActionListener {
+public class MenuMnemonicDemo implements ActionListener {
 
     JLabel jLabel;
 
-    MenuDemo() {
+    MenuMnemonicDemo() {
         // Создать новый контейнер типа JFrame
-        JFrame jFrame = new JFrame("Main Menu"); // Демонстрация меню
+        JFrame jFrame = new JFrame("Main Menu Mnemonic"); // Демонстрация меню
 
         // Указать диспетчер потоковой компановки типа FlowLayout
         jFrame.setLayout(new FlowLayout());
@@ -37,11 +39,20 @@ public class MenuDemo implements ActionListener {
         JMenuBar menuBar = new JMenuBar();
 
         // Создать меню File
-        JMenu jmFile = new JMenu("File");                 // Файл
-        JMenuItem jmiOpen = new JMenuItem("Open");      // Открыть
-        JMenuItem jmiClose = new JMenuItem("Close");    // Закрыть
-        JMenuItem jmiSave = new JMenuItem("Save");      // Сохранить
-        JMenuItem jmiExit = new JMenuItem("Exit");      // Выйти
+        JMenu jmFile = new JMenu("File");                                // Файл
+        jmFile.setMnemonic(KeyEvent.VK_F);
+
+        JMenuItem jmiOpen = new JMenuItem("Open", KeyEvent.VK_O);      // Открыть
+        jmiOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
+
+        JMenuItem jmiClose = new JMenuItem("Close", KeyEvent.VK_C);    // Закрыть
+        jmiClose.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
+
+        JMenuItem jmiSave = new JMenuItem("Save", KeyEvent.VK_S);      // Сохранить
+        jmiSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+
+        JMenuItem jmiExit = new JMenuItem("Exit", KeyEvent.VK_E);      // Выйти
+        jmiExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_DOWN_MASK));
 
         jmFile.add(jmiOpen);
         jmFile.add(jmiClose);
@@ -130,6 +141,6 @@ public class MenuDemo implements ActionListener {
 
     public static void main(String[] args) {
         // Создать фрейм в потоке деспетчиризации событий
-        SwingUtilities.invokeLater(MenuDemo::new);
+        SwingUtilities.invokeLater(MenuMnemonicDemo::new);
     }
 }
